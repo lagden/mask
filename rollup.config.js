394c610d1922fbd5505ed23d2e8963c37259a384
Thiago Lagden <lagden@gmail.com>
2020-10-26 17:19:31 -0300

    fix build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,15 @@
 'use strict'
 
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+
+function plugins() {
+	return [
+		resolve({browser: true}),
+		commonjs()
+	]
+}
+
 const config = [
 	{
 		input: 'src/index.js',
@@ -10,14 +20,22 @@ const config = [
 				name: 'Mask',
 				sourcemap: true,
 				strict: false
-			}, {
+			}
+		],
+		external: ['@tadashi/hex-id']
+	},
+	{
+		input: 'src/index.js',
+		output: [
+			{
 				file: 'dist/index.umd.js',
 				format: 'umd',
 				name: 'Mask',
 				sourcemap: true,
 				strict: false
 			}
-		]
+		],
+		plugins: plugins()
 	}
 ]
 
